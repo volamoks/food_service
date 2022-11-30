53df7fe2558386e34842262e218a82b864577cb6
volamoks <114908134+volamoks@users.noreply.github.com>
2022-11-30 21:17:31 +0500

    Modal updated by adding while scrolling

--- js/script.js
+++ js/script.js
@@ -66,8 +66,7 @@ document.addEventListener("DOMContentLoaded", () => {
     };
   }
 
-  function getZero(num
-    ) {
+  function getZero(num) {
     if (num < 10 && num >= 0) {
       return `0${num}`;
     } else {
@@ -105,35 +104,47 @@ document.addEventListener("DOMContentLoaded", () => {
     modal = document.querySelector(".modal"),
     modalCloseBtn = document.querySelector("[data-close]");
 
-  modalTrigger.forEach((btn) =>{
-    btn.addEventListener("click", () => {
-    // modal.classList.add("show"), 
-    // modal.classList.remove("hide");\
-    modal.classList.toggle('show')
-    document.body.style.overflow ='hidden'
-  })
-  
-  });
+  function openModal() {
+    modal.classList.toggle("show");
+    document.body.style.overflow = "hidden";
+    clearInterval(modalTimerId);
+  }
 
   function closeModal() {
-      modal.classList.toggle('show')
-      document.body.style.overflow ='';
-
+    modal.classList.toggle("show");
+    document.body.style.overflow = "";
   }
+
+  modalTrigger.forEach((btn) => {
+    btn.addEventListener("click", openModal);
+  });
+
   modalCloseBtn.addEventListener("click", closeModal);
-  
-modal.addEventListener('click', (e)=>{
-  if(e.target === modal ){
-    closeModal()
-  }
-})
 
-document,addEventListener('keydown', (e) =>{
-  if (e.code === 'Escape' && modal.classList.contains('show')) {
-    closeModal();
-  }
-})  
+  modal.addEventListener("click", (e) => {
+    if (e.target === modal) {
+      closeModal();
+    }
+  });
 
+  document,
+    addEventListener("keydown", (e) => {
+      if (e.code === "Escape" && modal.classList.contains("show")) {
+        closeModal();
+      }
+    });
+
+  const modalTimerId = setTimeout(openModal, 5000);
+
+  function showModalByScroll() {
+    if (
+      window.pageYOffset + document.documentElement.clientHeight >=
+      document.documentElement.scrollHeight - 1
+    ) {
+      openModal();
+      window.removeEventListener("scroll", showModalByScroll);
+    }
+  }
+
+  window.addEventListener("scroll", showModalByScroll);
 });
-
-
